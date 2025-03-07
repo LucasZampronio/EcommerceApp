f8b7f0fd6b33e712e6a87cab0bb7d04bb6cf02c6
Lucas Zampronio <lucas.zampronio.silva@gmail.com>
2025-03-07 05:20:33 -0300

    Feat('Home first section')

--- webcommerce/src/pages/home.tsx
+++ webcommerce/src/pages/home.tsx
@@ -1,27 +1,30 @@
 
 import Header from '../components/Header'
 import RightArrow from '../Images/Icons/RightArrowWhite.svg'
-import Guyimage from '../Images/Hero Image.png'
+import Guyimage from '../Images/Hero Image.svg'
+import Star from '../Images/Estrela.svg'
+import Background from '../Images/Background.svg'
 function Home() {
   return (
     <div>
         <Header />
         <section className='bg-neutral-100 w-full h-140'>
-          <div className='flex font-[Inter]'>
-            <div>
+          <div className='flex font-[Inter] border-2 items-center justify-between  border-red-700 w-full h-141'>
+            <div className='ml-37'>
               <h1 className='text-neutral-800 font-bold text-[32px]'>Fresh Arrivals Online</h1>
-              <h2 className='text-neutral-600 font-normal tex-[14px]'>Discover Our Newest Collection Today.</h2>
+              <h2 className='text-neutral-600 font-normal tex-[14px] mt-5'>Discover Our Newest Collection Today.</h2>
 
-              <div className=' border-2 rounded-[6px] gap-4 flex bg-neutral-900 w-46 justify-center items-center pt-2 pb-2'>
-                <p className='font-medium text-[14px] text-neutral-100'>View Collection</p>
+              <div className=' border-2 rounded-[6px] mt-16 gap-4 flex bg-neutral-900 w-50 justify-center items-center pt-2 pb-2'>
+                <button className='font-medium text-[14px] text-neutral-100'>View Collection</button>
                 <img src={RightArrow} alt="" />
-              </div>
-              <div className='flex'>
-                <img src={Guyimage} alt="" />
               </div>
             </div>
             <div>
-
+            <div className='grid grid-cols-1 grid-rows-1 border-2 h-141 border-red-700 mr-20'>
+              <img className='col-start-1 col-end-2 row-start-1 row-end-2 z-10 self-start translate-y-25 -translate-x-20' src={Star} alt="Star" />
+              <img className='col-start-1 col-end-2 row-start-1 row-end-2 w-110 h-auto self-end z-0 -translate-x-24 -translate-y-6' src={Background} alt="Background" />
+              <img className='col-start-1 col-end-2 row-start-1 row-end-2 w-86 h-auto translate-y-14 z-20' src={Guyimage} alt="Model" />
+            </div>
             </div>
           </div>
           <div>
